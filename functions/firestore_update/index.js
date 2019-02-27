c85673ed6508eb5cb3bcb80d4bf473f945504fb4
Charlie Wang <chwa@chwa-macbookpro.roam.corp.google.com>
2019-02-27 23:53:02 +0100

    clean firestore function

--- functions/firestore_update/index.js
+++ functions/firestore_update/index.js
@@ -1,72 +1,21 @@
-const {google} = require('googleapis');
 const functions = require('firebase-functions');
-exports.updateFunc = functions.firestore
-    .document('devices/{deviceId}/config/{configId}')
-    .onUpdate((change, context) => {
-      const newValue = change.after.data();
+const iot = require('@google-cloud/iot');
+const client = new iot.v1.DeviceManagerClient();
 
-      getClient(client => {
-        setDeviceConfig(
-          client,
-          context.params.configId,
-          '<registry_id>',
-          '<project_id>',
-          'us-central1',
-          JSON.stringify(newValue, null, 2),
-          '0'
-        )
-      })
-    });
-
-function setDeviceConfig(
-  client,
-  deviceId,
-  registryId,
-  projectId,
-  cloudRegion,
-  data,
-  version
-) {
-  const parentName = `projects/${projectId}/locations/${cloudRegion}`;
-  const registryName = `${parentName}/registries/${registryId}`;
-
-  const binaryData = Buffer.from(data).toString('base64');
-  const request = {
-    name: `${registryName}/devices/${deviceId}`,
-    versionToUpdate: version,
-    binaryData: binaryData,
-  };
-
-  client.projects.locations.registries.devices.modifyCloudToDeviceConfig(
-    request,
-    (err, data) => {
-      if (err) {
-        console.log('Could not update config:', deviceId);
-        console.log('Message: ', err);
-      } else {
-        console.log('Success :', data);
-      }
-    }
-  );
-  // [END iot_set_device_config]
-}
-
-function getClient(cb) {
-  google.auth.getApplicationDefault()
-  .then(res => {
-    let auth = res.credential;
-    if (auth.createScopedRequired && auth.createScopedRequired()) {
-      const scopes = ['https://www.googleapis.com/auth/cloud-platform'];
-      auth = auth.createScoped(scopes);
-    }
-    client = google.cloudiot({
-      version: 'v1',
-      auth: auth
-    });
-    cb(client);
-	  return 0;
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
+exports.onFirestoreUpdate = functions.firestore
+  .document('devices/{deviceId}/config/{configId}')
+  .onUpdate((change, context) => {
+    let newValue = change.after.data();
+    let data = JSON.stringify(newValue, null, 2);
+    return updateDeviceConfig(context.params.configId, data);
   });
+
+function updateDeviceConfig(deviceId, data) {
+  let formattedName = client.devicePath('<project_id>', '<region>', '<registry>', deviceId);
+  let binaryData = Buffer.from(data).toString('base64');
+  let request = {
+    name: formattedName,
+    binaryData: binaryData
+  };
+  return client.modifyCloudToDeviceConfig(request);
 }
--- registry_setup.md
+++ registry_setup.md
@@ -104,14 +104,18 @@ gcloud beta functions deploy eventUpdate \
 
 ## Create cloud function for sending Firestore updates to device
 ```bash
-mkdir firestore_upate
+mkdir firestore_update
 cd firestore_update
-npm install firebase-functions@latest firebase-admin@latest --save
+npm install firebase-functions@latest firebase-admin@latest @google-cloud/iot@latest --save
 npm install -g firebase-tools
 firebase login
 firebase init functions
+cd functions
+npm install @google-cloud/iot@latest
+cd ..
 ```
-Override the `index.js`  functions with the one in this directory and also make sure to substitute `<project_d>` and  `<registry_id>`
+Override the `index.js`  functions with the one in this directory and also make sure to substitute `<project_d>`, `<registry>` and `<region>`
+Deploy the function
 ```bash
 firebase deploy --only functions
 ```
