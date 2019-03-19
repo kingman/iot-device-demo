f59bc971d65912cbd6d905d9eaec6eefd36f8f18
Charlie Wang <chwa@chwa-macbookpro.roam.corp.google.com>
2019-03-19 12:59:22 +0100

    formatting bug

--- functions/firestore_update/index.js
+++ functions/firestore_update/index.js
@@ -3,7 +3,7 @@ const iot = require('@google-cloud/iot');
 const client = new iot.v1.DeviceManagerClient();
 
 exports.onFirestoreUpdate = functions.firestore
-  .document('${functions.config().device.database.name}/{deviceId}')
+  .document(`${functions.config().device.database.name}/{deviceId}`)
   .onUpdate((change, context) => {
     let newValue = change.after.data();
     let data = JSON.stringify(newValue, null, 2);
@@ -11,7 +11,7 @@ exports.onFirestoreUpdate = functions.firestore
   });
 
 function updateDeviceConfig(deviceId, data) {
-  let formattedName = client.devicePath('${functions.config().iot.core.projectid}', '${functions.config().iot.core.region}', '${functions.config().iot.core.registry}', deviceId);
+  let formattedName = client.devicePath(functions.config().iot.core.projectid, functions.config().iot.core.region, functions.config().iot.core.registry, deviceId);
   let binaryData = Buffer.from(data).toString('base64');
   let request = {
     name: formattedName,
